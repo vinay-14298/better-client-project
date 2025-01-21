e828809e631c761fa2da38cd1a5b6d820ba5ff02
root <root@ip-10-0-0-32.us-east-2.compute.internal>
2025-01-21 14:07:04 +0000

    modified Dockerfile and app.py

--- app/app.py
+++ app/app.py
@@ -103,5 +103,5 @@ def register_success():
     return render_template('register_success.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=80)
 
